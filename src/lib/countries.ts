b87a36754751d84cd45e503c330e6d9ad958c338
anwarabduallah1-max <anwarabduallah1@gmail.com>
2026-08-15 10:31:37 +0300

    Updated countries.ts

--- src/lib/countries.ts
+++ src/lib/countries.ts
@@ -86,7 +86,7 @@ export const COUNTRIES: Country[] = [
   { code: 'IR', name: 'Iran' },
   { code: 'IQ', name: 'Iraq' },
   { code: 'IE', name: 'Ireland' },
-  { code: 'IL', name: 'Israel' },
+
   { code: 'IT', name: 'Italy' },
   { code: 'JM', name: 'Jamaica' },
   { code: 'JP', name: 'Japan' },
